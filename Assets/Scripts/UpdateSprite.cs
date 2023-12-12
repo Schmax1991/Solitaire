bf93110b5f891a0df63e62bbc4b91b377bb36da7
Schmax1991 <maximilyan1991@gmail.com>
2023-12-12 22:38:28 +0300

    First commit

---
+++ Assets/Scripts/UpdateSprite.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.IO;
+
+public class UpdateSprite : MonoBehaviour
+{
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private SpriteRenderer spriteRenderer;
+    private Selectable selectable;
+    private Solitaire solitaire;
+
+
+    void Start()
+    {
+        List<string> deck = Solitaire.GenerateDeck();
+        solitaire = FindObjectOfType<Solitaire>();
+
+        int i = 0;
+        foreach(string card in deck)
+        {
+            if(this.name == card)
+            {
+                cardFace = solitaire.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<Selectable>();
+    }
+
+    
+    void Update()
+    {
+       if(selectable.faceUp == true)
+       {
+            spriteRenderer.sprite = cardFace;
+       } 
+       else
+        spriteRenderer.sprite = cardBack;
+    }
+}
